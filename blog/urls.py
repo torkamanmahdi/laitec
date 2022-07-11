d972c049f15c1a780dbdfbf7806a4a5efa928a29
Mahdi Torkaman <mahditorkaman@pm.me>
2022-07-11 17:33:59 +0430

    phase5

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('create/', views.create_post, name='create_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def create_post(request):
+    from .models import Post
+    from django.contrib.auth.models import User
+    from random import randrange as rd
+    me = User.objects.get(username='admin')
+    for i in range(50):
+        x = rd(100000)
+        Post.objects.create(author=me, title=f'Sample title {i}', text=f'Test {i} {x}')
+    return HttpResponse('done!')
