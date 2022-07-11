d94bd54a843fa4ea5f3a4e746f73b870a1c467ed
Mahdi Torkaman <mahditorkaman@pm.me>
2022-07-11 18:43:08 +0430

    phase7

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def create_post(request):
-    from .models import Post
     from django.contrib.auth.models import User
     from random import randrange as rd
     me = User.objects.get(username='admin')
     for i in range(50):
-        x = rd(100000)
-        Post.objects.create(author=me, title=f'Sample title {i}', text=f'Test {i} {x}')
+        x = timezone.now()
+        a = Post.objects.create(author=me, title=f'Sample title {i} {x}', text=f'Test {i} {x}')
+        a.publish()
     return HttpResponse('done!')
